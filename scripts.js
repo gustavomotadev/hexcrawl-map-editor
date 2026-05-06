9f9c03046c3a6f6b442876a80f7c9bc2a64bc8c7
gustavomotadev <gustavomotadev@gmail.com>
2026-05-06 11:48:45 -0300

    Code refactoring

--- scripts.js
+++ scripts.js
@@ -2,16 +2,54 @@ import * as honeycomb from "https://esm.sh/honeycomb-grid@4.1.5";
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import pickr from "https://cdn.jsdelivr.net/npm/@simonwep/pickr/+esm";
 
+const Tool = Object.freeze({
+  BRUSH: "BRUSH",
+  EYEDROPPER: "EYEDROPPER"
+});
+
+const AppState = {
+  colorPicker: null,
+  grid: null,
+  mapGroup: null,
+};
+
 document.addEventListener("DOMContentLoaded", () => {
 
-  const grid = createGrid(30, 20, honeycomb.Orientation.FLAT);
+  AppState.grid = createGrid(30, 20, honeycomb.Orientation.FLAT);
 
-  quickLoad(grid);
+  quickLoad();
+
+  AppState.mapGroup = d3Setup();
+
+  renderAllHexes();
+
+  pickrSetup();
+
+  localStorageSetup();
+
+  toolTraySetup();
+
+});
+
+function createGrid(hexWidth, hexHeight, hexOrientation, hexDimension=30, hexFillColor="#000", hexStrokeColor="#aaa") {
+
+  const HexClass = honeycomb.defineHex({ dimensions: hexDimension, orientation: hexOrientation });
+  const grid = new honeycomb.Grid(HexClass, honeycomb.rectangle({ width: hexWidth, height: hexHeight }));
+
+  grid.forEach(hex => {
+    hex.fillColor = hexFillColor; // Set a default fill color
+    hex.strokeColor = hexStrokeColor; // Set a default stroke color
+  });
+
+  return grid
+}
+
+function d3Setup() {
 
   const width = 500;
   const height = 500;
 
-  // 2. Setup the SVG Canvas via D3
+  // Setup the SVG Canvas via D3
   const svg = d3.select("#app")
     .append("svg")
     .attr("width", "100%")
@@ -23,25 +61,25 @@ document.addEventListener("DOMContentLoaded", () => {
     .attr("id", "map-group")
     .attr("transform", "translate(45, 45)"); // Starting offset
 
-  // ==========================================
-  // 3. DEFINE PAN AND ZOOM BEHAVIOR
-  // ==========================================
+
+  // DEFINE PAN AND ZOOM BEHAVIOR
   const zoom = d3.zoom()
     // Restricts how far out (0.25x) and how far in (4x) the user can zoom
     .scaleExtent([0.25, 4])
     // The zoom event updates the transform attribute of our #map-group
-    .on("zoom", (event) => {
-      mapGroup.attr("transform", event.transform);
-    });
+    .on("zoom", (event) => {mapGroup.attr("transform", event.transform);});
 
   // Attach the zoom event handler to the top-level SVG
   svg.call(zoom);
 
-  // ==========================================
+  return mapGroup;
+}
 
-  // 4. Render initial hexes
-  mapGroup.selectAll("polygon.hex-tile")
-    .data(Array.from(grid))
+function renderAllHexes() {
+
+  // Render all hexes
+  AppState.mapGroup.selectAll("polygon.hex-tile")
+    .data(Array.from(AppState.grid))
     .enter()
     .append("polygon")
     .attr("class", "hex-tile")
@@ -49,43 +87,24 @@ document.addEventListener("DOMContentLoaded", () => {
     .style("fill", (hex) => hex.fillColor)
     .style("stroke", (hex) => hex.strokeColor)
     .style("stroke-width", "1.5px")
-    .on("click", function (event, hex) {
+    .on("click", hexClickHandler);
+}
 
-      if (tool === 'eyedropper') {
+function setTool(tool) {
+  localStorage.setItem('currentTool', tool);
+}
 
-        // const currentFill = d3.select(this).style("fill");
-        brushColor = hex.fillColor;
-        tool = 'brush';
-        toggleButton('btn-eyedropper');
-        colorPicker.setColor(brushColor);
-        btnColor.style.color = brushColor;
+function getTool() {
+  return localStorage.getItem('currentTool');
+}
 
-      } else {
+function pickrSetup() {
 
-        hex.fillColor = brushColor;
-        d3.select(this).style("fill", brushColor);
-
-        quickSave(grid);
-      }
-
-      console.debug(`Hex clicked: q=${hex.q}, r=${hex.r}`);
-    });
-
-  // console.debug(`Grid rendered. Use mouse wheel, click + drag, or pinch gestures to zoom and pan!`);
-
-  // color button
-
-  let brushColor = "#4CAF50";
-  const btnColor = document.getElementById("btn-color");
-  btnColor.style.color = brushColor;
-
-  // pickr
-
-  const colorPicker = pickr.create({
+  AppState.colorPicker = pickr.create({
     el: '#btn-color',
     useAsButton: true,
     theme: 'nano', // or 'classic', or 'monolith'
-    default: brushColor, // Default color
+    default: getBrushColor(), // Default color
 
     swatches: [
       '#1b5d12', // dark green
@@ -121,22 +140,70 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Example of how to use the selected color
-  colorPicker.on('change', (color, instance) => {
-    brushColor = color.toHEXA().toString();
-    btnColor.style.color = brushColor;
+  AppState.colorPicker.on('change', (color, instance) => {
+    setBrushColor(color.toHEXA().toString());
+    // brushColor = color.toHEXA().toString();
+    // btnColor.style.color = brushColor;
     // console.debug('Selected color for painting:', brushColor);
     // You can now set a global variable like 'currentBrushColor' to this value
     // instance.hide();
   });
+}
+
+function setBrushColor(color) {
+  localStorage.setItem('brushColor', color);
+  const btnColor = document.getElementById("btn-color");
+  btnColor.style.color = color;
+  AppState.colorPicker.setColor(color);
+}
+
+function getBrushColor() {
+  return localStorage.getItem('brushColor');
+}
+
+function hexClickHandler(event, hex) {
+
+  if (getTool() === Tool.EYEDROPPER) {
+
+    // const currentFill = d3.select(this).style("fill");
+    setBrushColor(hex.fillColor);
+    setTool(Tool.BRUSH);
+    toggleButton('btn-eyedropper');
+    // colorPicker.setColor(brushColor);
+    // btnColor.style.color = brushColor;
+
+  } else {
+
+    hex.fillColor = getBrushColor();
+    d3.select(this).style("fill", getBrushColor());
+
+    quickSave();
+  }
+
+  console.debug(`Hex clicked: q=${hex.q}, r=${hex.r}`);
+}
+
+function localStorageSetup() {
+
+  if (!getBrushColor()) {
+    setBrushColor("#4CAF50");
+  } else {
+    setBrushColor(getBrushColor());
+  }
+
+  if (!getTool()) {
+    setTool(Tool.BRUSH);
+  }
+}
+
+function toolTraySetup() {
 
   // eyedropper
-
-  let tool = 'brush';
 
   const btnEyedropper = document.getElementById('btn-eyedropper');
   btnEyedropper.addEventListener('click', (e) => {
 
-    tool = 'eyedropper';
+    setTool(Tool.EYEDROPPER);
     toggleButton('btn-eyedropper');
 
   });
@@ -149,30 +216,6 @@ document.addEventListener("DOMContentLoaded", () => {
   //   brushColor = '#000000';
 
   // });
-
-});
-
-function createGrid(hexWidth, hexHeight, hexOrientation, hexDimension=30, hexFillColor="#000", hexStrokeColor="#aaa") {
-
-  const HexClass = honeycomb.defineHex({ dimensions: hexDimension, orientation: hexOrientation });
-
-
-  //
-  const testHex = new HexClass();
-  console.log("Orientation:", testHex.orientation); 
-  console.log("Corners:", testHex.corners);
-  console.log("Width:", testHex.width, "Height:", testHex.height);
-  //
-
-
-  const grid = new honeycomb.Grid(HexClass, honeycomb.rectangle({ width: hexWidth, height: hexHeight }));
-
-  grid.forEach(hex => {
-    hex.fillColor = hexFillColor; // Set a default fill color
-    hex.strokeColor = hexStrokeColor; // Set a default stroke color
-  });
-
-  return grid
 }
 
 function toggleButton(buttonId) {
@@ -196,15 +239,13 @@ function compareColor(color1, color2) {
   return c1.r === c2.r && c1.g === c2.g && c1.b === c2.b;
 }
 
-function quickSave(grid) {
+function quickSave() {
 
-  const fullMapData = Array.from(grid).map(hex => ({ q: hex.q, r: hex.r, color: hex.fillColor }));
+  const fullMapData = Array.from(AppState.grid).map(hex => ({ q: hex.q, r: hex.r, color: hex.fillColor }));
 
   const filteredMapData = fullMapData.filter((hex) => (!compareColor(hex.color, "#000")));
 
-  const gridOrientation = Array.from(grid)[0].orientation;
-
-  console.debug('here: ', gridOrientation);
+  const gridOrientation = Array.from(AppState.grid)[0].orientation;
 
   let saveData = { orientation: gridOrientation, palette: [], hexes: {} };
 
@@ -222,7 +263,7 @@ function quickSave(grid) {
   localStorage.setItem('quickSaveData', saveDataString);
 }
 
-function quickLoad(grid) {
+function quickLoad() {
 
   const quickSaveJSON = localStorage.getItem('quickSaveData'); 
 
@@ -232,7 +273,7 @@ function quickLoad(grid) {
 
   const quickSaveObject = JSON.parse(quickSaveJSON);
 
-  const currentOrientation = Array.from(grid)[0].orientation;
+  const currentOrientation = Array.from(AppState.grid)[0].orientation;
   if (currentOrientation !== quickSaveObject.orientation) {
     return false;
   }
@@ -245,8 +286,8 @@ function quickLoad(grid) {
     const r = Number(qrArray[1]);
     const color = quickSaveObject.palette[colorIndex];
 
-    console.debug(q, r);
-    const targetHex = grid.getHex([q, r]);
+    // console.debug(q, r);
+    const targetHex = AppState.grid.getHex([q, r]);
 
     // console.debug(color);
     targetHex.fillColor = color;
